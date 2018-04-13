f2c4d35a61772920113e9d52c74c170478ecf984
OVCoder <citryn85@gmail.com>
2018-04-13 16:18:04 -0700

    updated datasources.production.js

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,7 +1,12 @@
 module.exports = {
   "MongoDB": {
+    "host": process.env.MONGODB_HOST,
+    "port": process.env.MONGODB_PORT,
+    "url": "",
+    "database": process.env.MONGODB_DATABASE,
+    "password": process.env.MONGODB_PASSWORD,
     "name": "MongoDB",
-    "connector": "mongodb",
-    "url": process.env.MONGODB_URI
+    "user": process.env.MONGODB_USERNAME,
+    "connector": "mongodb"
   }
 }
